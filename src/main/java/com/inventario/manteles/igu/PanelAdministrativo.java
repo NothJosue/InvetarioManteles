f2308ea51011f31775cce37be20696862eb40660
NothJosue <thedarkm04@gmail.com>
2025-08-13 19:02:16 -0500

    Agregar funcionalidad de reportes: Manteles, Clientes y Compras

--- src/main/java/com/inventario/manteles/igu/PanelAdministrativo.java
+++ src/main/java/com/inventario/manteles/igu/PanelAdministrativo.java
@@ -1,9 +1,26 @@
 package com.inventario.manteles.igu;
 
+import com.inventario.manteles.modelo.Cliente;
+import com.inventario.manteles.modelo.Compra;
+import com.inventario.manteles.modelo.Controladora;
+import com.inventario.manteles.modelo.MesaACrochet;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.UnitValue;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class PanelAdministrativo extends javax.swing.JFrame {
 
+    Controladora control;
 
     public PanelAdministrativo() {
+        control = new Controladora();
         initComponents();
         this.setExtendedState(this.MAXIMIZED_BOTH);
         this.setLocationRelativeTo(null);
@@ -30,6 +47,7 @@ public class PanelAdministrativo extends javax.swing.JFrame {
         menuReportes = new javax.swing.JMenu();
         subMenuReportesManteles = new javax.swing.JMenuItem();
         subMenuReportesClientes = new javax.swing.JMenuItem();
+        subMenuReportesCompras = new javax.swing.JMenuItem();
         menuHistorial = new javax.swing.JMenu();
         subMenuHistorial = new javax.swing.JMenuItem();
         menuCerrarSesion = new javax.swing.JMenu();
@@ -166,6 +184,15 @@ public class PanelAdministrativo extends javax.swing.JFrame {
         });
         menuReportes.add(subMenuReportesClientes);
 
+        subMenuReportesCompras.setIcon(new javax.swing.ImageIcon("D:\\Programacion\\Java\\Proyectos Portafolio\\InventarioManteles\\src\\main\\Resources\\iconRepotesGenerales.png")); // NOI18N
+        subMenuReportesCompras.setText("Reportes Compras");
+        subMenuReportesCompras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                subMenuReportesComprasActionPerformed(evt);
+            }
+        });
+        menuReportes.add(subMenuReportesCompras);
+
         jMenuBar1.add(menuReportes);
 
         menuHistorial.setIcon(new javax.swing.ImageIcon("D:\\Programacion\\Java\\Proyectos Portafolio\\InventarioManteles\\src\\main\\Resources\\iconHistorial.png")); // NOI18N
@@ -193,6 +220,11 @@ public class PanelAdministrativo extends javax.swing.JFrame {
         subMenuCerrarSesion.setIcon(new javax.swing.ImageIcon("D:\\Programacion\\Java\\Proyectos Portafolio\\InventarioManteles\\src\\main\\Resources\\iconCerrarSesion.png")); // NOI18N
         subMenuCerrarSesion.setText("Cerrar Sesión");
         subMenuCerrarSesion.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        subMenuCerrarSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                subMenuCerrarSesionActionPerformed(evt);
+            }
+        });
         menuCerrarSesion.add(subMenuCerrarSesion);
 
         jMenuBar1.add(menuCerrarSesion);
@@ -203,7 +235,38 @@ public class PanelAdministrativo extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void subMenuReportesClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuReportesClientesActionPerformed
-        // TODO add your handling code here:
+        String ruta = "D:/Reportes_Clientes.pdf";
+        try {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(ruta));
+            Document documento = new Document(pdf);
+            Table table = new Table(UnitValue.createPercentArray(7)).useAllAvailableWidth();
+            final String[] columnasManteles = {" ", "Nombres y Apellidos", "Telefono", "Distrito", "Dirección", "Referencia", "Fecha de Visita", "Deuda"};
+            int numero_de_filas = 1;
+
+            // Agregar nombre de las columnas
+            for (int i = 0; i < 7; i++) {
+                table.addCell(columnasManteles[i]);
+            }
+            // Agregar las filas
+            List<Cliente> listaClientes = control.traerClientes();
+            for (Cliente cliente : listaClientes) {
+                table.addCell(String.valueOf(numero_de_filas));
+                table.addCell(cliente.getNombres() + " " + cliente.getApellidos());
+                table.addCell(cliente.getDistrito());
+                table.addCell(cliente.getDireccion());
+                table.addCell(cliente.getReferencia());
+                table.addCell(String.valueOf(cliente.getFecha_visita()));
+                table.addCell(String.valueOf(cliente.getDeuda() + " S/."));
+                numero_de_filas++;
+            }
+
+            documento.add(table);
+            documento.close();
+            JOptionPane.showMessageDialog(null, "Generación del reporte de clientes exitoso", "Reporte exitoso", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (IOException ex) {
+            Logger.getLogger(PanelAdministrativo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_subMenuReportesClientesActionPerformed
 
     private void subMenuNuevoMantelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuNuevoMantelActionPerformed
@@ -219,9 +282,9 @@ public class PanelAdministrativo extends javax.swing.JFrame {
     }//GEN-LAST:event_subMenuNuevoUsuarioActionPerformed
 
     private void subMenuNuevoClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuNuevoClienteActionPerformed
-       NuevoCliente pantallaNuevoCliente = new NuevoCliente();
-       panelEscritorio.add(pantallaNuevoCliente);
-       pantallaNuevoCliente.setVisible(true);
+        NuevoCliente pantallaNuevoCliente = new NuevoCliente();
+        panelEscritorio.add(pantallaNuevoCliente);
+        pantallaNuevoCliente.setVisible(true);
     }//GEN-LAST:event_subMenuNuevoClienteActionPerformed
 
     private void subMenuGestionarUsuariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuGestionarUsuariosActionPerformed
@@ -243,19 +306,100 @@ public class PanelAdministrativo extends javax.swing.JFrame {
     }//GEN-LAST:event_subMenuGestionarClientesActionPerformed
 
     private void subMenuAsignarProductoUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuAsignarProductoUsuarioActionPerformed
-       AsignarProductoUsuario pantallaAsignar = new AsignarProductoUsuario();
-       panelEscritorio.add(pantallaAsignar);
-       pantallaAsignar.setVisible(true);
+        AsignarProductoUsuario pantallaAsignar = new AsignarProductoUsuario();
+        panelEscritorio.add(pantallaAsignar);
+        pantallaAsignar.setVisible(true);
     }//GEN-LAST:event_subMenuAsignarProductoUsuarioActionPerformed
 
     private void subMenuReportesMantelesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuReportesMantelesActionPerformed
-        // TODO add your handling code here:
+        String ruta = "D:/Reportes_Manteles.pdf";
+        try {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(ruta));
+            Document documento = new Document(pdf);
+            Table table = new Table(UnitValue.createPercentArray(7)).useAllAvailableWidth();
+            final String[] columnasManteles = {" ", "Forma", "Color", "# sillas", "Tamaño", "Precio", "Stock"};
+            int numero_de_filas = 1;
+            /**
+             *
+             * COLUMNAS 1 -> Numero de productos 2 -> Forma del mandel 3 ->
+             * Color del mantel 4 -> Numero de sillas 5 -> Tamaño 6 -> Precio 7
+             * -> Stock
+             *
+             */
+            // Agregar nombre de las columnas
+            for (int i = 0; i < 7; i++) {
+                table.addCell(columnasManteles[i]);
+            }
+            // Agregar las filas
+            List<MesaACrochet> listaMantelesMesa = control.traerListaMesas();
+            for (MesaACrochet mantelMesa : listaMantelesMesa) {
+                table.addCell(String.valueOf(numero_de_filas));
+                table.addCell(mantelMesa.getForma());
+                table.addCell(mantelMesa.getColor());
+                table.addCell(String.valueOf(mantelMesa.getNumeroSillas()));
+                table.addCell(String.valueOf(mantelMesa.getTamanio()) + "cm");
+                table.addCell(String.valueOf(mantelMesa.getPrecio()) + " S/.");
+                table.addCell(String.valueOf(mantelMesa.getCantidad()));
+                numero_de_filas++;
+            }
+
+            documento.add(table);
+            documento.close();
+            JOptionPane.showMessageDialog(null, "Generación del reporte de manteles exitoso", "Reporte exitoso", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (IOException ex) {
+            Logger.getLogger(PanelAdministrativo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_subMenuReportesMantelesActionPerformed
 
     private void subMenuHistorialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuHistorialActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_subMenuHistorialActionPerformed
-    
+
+    private void subMenuCerrarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuCerrarSesionActionPerformed
+        IniciarSesion pantallaInicioIniciarSesion = new IniciarSesion();
+        JOptionPane.showMessageDialog(null, "Cerro sesión exitosamente :D", "Cerrado Exitoso", JOptionPane.INFORMATION_MESSAGE);
+        this.dispose();
+        pantallaInicioIniciarSesion.setVisible(true);
+        pantallaInicioIniciarSesion.setLocationRelativeTo(null);
+    }//GEN-LAST:event_subMenuCerrarSesionActionPerformed
+
+    private void subMenuReportesComprasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuReportesComprasActionPerformed
+        String ruta = "D:/Reportes_Compras.pdf";
+        try {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(ruta));
+            Document documento = new Document(pdf);
+            Table table = new Table(UnitValue.createPercentArray(7)).useAllAvailableWidth();
+            final String[] columnasManteles = {" ", "Cliente", "Mantel", "Fecha de Compra", "Cantidad", "Precio Unitario", "Precio Total",};
+            int numero_de_filas = 1;
+
+            // Agregar nombre de las columnas
+            for (int i = 0; i < 7; i++) {
+                table.addCell(columnasManteles[i]);
+            }
+            // Agregar las filas
+            List<Compra> listaCompra = control.traerCompras();
+            for (Compra compra : listaCompra) {
+                table.addCell(String.valueOf(numero_de_filas));
+                table.addCell(compra.getCliente().getNombres() + " " + compra.getCliente().getApellidos());
+                table.addCell(compra.getMantel().getForma() + " " + compra.getMantel().getColor() + " " + String.valueOf(compra.getMantel().getNumeroSillas()) + " sillas");
+                table.addCell(String.valueOf(compra.getFechaCompra()));
+                table.addCell(String.valueOf(compra.getCantidad()));
+                table.addCell(String.valueOf(compra.getPrecioUnitario() + " S/."));
+                table.addCell(String.valueOf(compra.getTotal() + " S/."));
+
+                numero_de_filas++;
+            }
+
+            documento.add(table);
+            documento.close();
+
+            JOptionPane.showMessageDialog(null, "Generación del reporte de compras exitoso", "Reporte exitoso", JOptionPane.INFORMATION_MESSAGE);
+        } catch (IOException ex) {
+            Logger.getLogger(PanelAdministrativo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_subMenuReportesComprasActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu Asignaciones;
     private javax.swing.JMenuBar jMenuBar1;
@@ -276,6 +420,7 @@ public class PanelAdministrativo extends javax.swing.JFrame {
     private javax.swing.JMenuItem subMenuNuevoMantel;
     private javax.swing.JMenuItem subMenuNuevoUsuario;
     private javax.swing.JMenuItem subMenuReportesClientes;
+    private javax.swing.JMenuItem subMenuReportesCompras;
     private javax.swing.JMenuItem subMenuReportesManteles;
     // End of variables declaration//GEN-END:variables
 }
